e0b49c79de0cdd2935817f40ab6b869fe3e175fe
Matt McCray <darthapo@gmail.com>
2014-03-21 12:32:05 -0500

    updated dist files.

--- component.js
+++ component.js
@@ -8,7 +8,7 @@
   var Component, React, extractInto, extractMethods, getFnName, ignoredKeys, nameParser, umd,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  ignoredKeys = '__super__ constructor reactify toComponent'.split(' ');
+  ignoredKeys = '__super__ __superConstructor__ constructor reactify toComponent'.split(' ');
 
   nameParser = /function (.+?)\(/;
 
