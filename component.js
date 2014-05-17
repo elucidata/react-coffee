e2570b37024951ef90ab3e7a4d78a58476153e16
Matt McCray <darthapo@gmail.com>
2014-05-17 12:34:22 -0500

    Added support for ignoring specified method names.

--- component.js
+++ component.js
@@ -5,7 +5,7 @@
  */
 
 (function() {
-  var Component, React, extractInto, extractMethods, getFnName, nameParser, umd,
+  var Component, React, extractAndMerge, extractInto, extractMethods, getFnName, handleMixins, handlePropTypes, nameParser, umd,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   nameParser = /function (.+?)\(/;
@@ -15,13 +15,16 @@
   Component = (function() {
     function Component() {}
 
-    Component.keyBlacklist = '__super__ __superConstructor__ constructor keyBlacklist reactify toComponent'.split(' ');
+    Component.keyBlacklist = '__super__ __superConstructor__ constructor keyBlacklist reactify build toComponent'.split(' ');
 
-    Component.toComponent = function(componentClass) {
+    Component.toComponent = function(componentClass, ignore) {
       if (componentClass == null) {
         componentClass = this;
       }
-      return React.createClass(extractMethods(componentClass));
+      if (ignore == null) {
+        ignore = [];
+      }
+      return React.createClass(extractMethods(componentClass, ignore));
     };
 
     Component.build = Component.toComponent;
@@ -32,27 +35,59 @@
 
   })();
 
-  extractMethods = function(comp) {
+  extractMethods = function(comp, ignore) {
     var methods;
-    methods = extractInto({}, new comp);
+    methods = extractInto({}, new comp, ignore);
     methods.displayName = getFnName(comp);
+    handleMixins(methods, comp);
+    handlePropTypes(methods, comp);
     methods.statics = extractInto({
       Class: comp
-    }, comp);
+    }, comp, ignore.concat(['mixins', 'propTypes']));
     return methods;
   };
 
-  extractInto = function(target, source) {
+  extractInto = function(target, source, ignore) {
     var key, val;
     for (key in source) {
       val = source[key];
       if (__indexOf.call(Component.keyBlacklist, key) >= 0) {
         continue;
       }
+      if (__indexOf.call(ignore, key) >= 0) {
+        continue;
+      }
       target[key] = val;
     }
     return target;
   };
+
+  extractAndMerge = function(prop, merge) {
+    return function(instance, statics) {
+      var result;
+      result = (typeof statics[prop] === "function" ? statics[prop]() : void 0) || statics[prop];
+      if (result != null) {
+        if (instance[prop] == null) {
+          return instance[prop] = result;
+        } else {
+          return instance[prop] = merge(instance[prop], result);
+        }
+      }
+    };
+  };
+
+  handlePropTypes = extractAndMerge('propTypes', function(target, value) {
+    var key, val;
+    for (key in value) {
+      val = value[key];
+      target[key] = val;
+    }
+    return target;
+  });
+
+  handleMixins = extractAndMerge('mixins', function(target, value) {
+    return target.concat(value);
+  });
 
   getFnName = function(fn) {
     return fn.name || fn.displayName || (fn.toString().match(nameParser) || [null, 'UnnamedComponent'])[1];
--- src/component.coffee
+++ src/component.coffee
@@ -7,29 +7,48 @@ React= @React or require('react')
 
 class Component
 
-  @keyBlacklist= '__super__ __superConstructor__ constructor keyBlacklist reactify toComponent'.split ' '
-  
-  @toComponent: (componentClass=this)->
-    React.createClass extractMethods componentClass
+  @keyBlacklist= '__super__ __superConstructor__ constructor keyBlacklist reactify build toComponent'.split ' '
+
+  @toComponent: (componentClass=this, ignore=[])->
+    React.createClass extractMethods componentClass, ignore
 
   @build: @toComponent
-  
+
   # DEPRECATED: This alias will be removed from a future version.
   @reactify: @toComponent
-  
 
-extractMethods= (comp)->
-  methods= extractInto {}, (new comp)
+
+extractMethods= (comp, ignore)->
+  methods= extractInto {}, (new comp), ignore
   methods.displayName= getFnName comp
-  methods.statics= extractInto Class:comp, comp
+  handleMixins methods, comp
+  handlePropTypes methods, comp
+  methods.statics= extractInto Class:comp, comp, ignore.concat(['mixins', 'propTypes'])
   methods
 
-extractInto= (target, source)->
+
+extractInto= (target, source, ignore)->
   for key,val of source
     continue if key in Component.keyBlacklist
+    continue if key in ignore
     target[key]= val
   target
 
+extractAndMerge= (prop, merge)->
+  (instance, statics)->
+    result= statics[prop]?() or statics[prop]
+    if result?
+      unless instance[prop]?
+        instance[prop]= result
+      else
+        instance[prop]= merge(instance[prop], result)
+
+handlePropTypes= extractAndMerge 'propTypes', (target, value)->
+  target[key]= val for key,val of value
+  target
+
+handleMixins= extractAndMerge 'mixins', (target, value)->
+  target.concat value
 
 getFnName= (fn)->
   fn.name or fn.displayName or (fn.toString().match(nameParser) or [null,'UnnamedComponent'])[1]
@@ -41,7 +60,7 @@ umd= (factory) ->
     define([], factory)
   else
     @Component = factory()
- 
+
 umd -> Component
 
 React.Component= Component
